fa2011a6131417ae9dae630e6623354cbcfed031
ntoto3 <thomas.noslen.edu@groupe-gema.com>
2026-03-30 14:11:21 +0200

    TP2 TP3 TP4 terminés

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+import redis
 
 app = Flask(__name__)
+r = redis.Redis(host="redis", port=6379)
 
-@app.route('/')
-def hello():
-    return "Hello DevOps"
+@app.route("/")
+def home():
+    r.incr("counter")
+    return f"Visites : {r.get('counter').decode()}"
 
 app.run(host="0.0.0.0", port=5000)
